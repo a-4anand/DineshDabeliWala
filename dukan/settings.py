8ad99cfcafc09afff04dbaac158b10a31a786e07
Anand kumar dubey <142786893+a-4anand@users.noreply.github.com>
2025-01-28 08:10:04 +0530

    fixed static file error

--- dukan/settings.py
+++ dukan/settings.py
@@ -150,6 +150,7 @@ STATIC_URL = "static/"
 #added manually
 
 STATIC_DIR = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'dabeli', 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
