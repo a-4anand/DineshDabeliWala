2e66153b607fe0b844394df5c2f8e3426e6b6417
Anand kumar dubey <142786893+a-4anand@users.noreply.github.com>
2025-03-23 12:24:07 +0530

    hehehehehhe

--- dukan/settings.py
+++ dukan/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "django-insecure--&()dt)+zdqa$sc+un^admluii%2_2bsynrz1382wjq1wh5b=d"
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [    "https://dineshdabeli.up.railway.app/",
     "https://dineshdabeli.great-site.net",
@@ -93,7 +93,8 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "dabeli", "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
